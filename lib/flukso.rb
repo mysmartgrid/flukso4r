dc4aa2402a28a9efb654806468f786e68228c347
Mathias Dalheimer <md@gonium.net>
2010-02-15 15:23:15 +0100

    changed url of flukso api to use https

--- lib/flukso.rb
+++ lib/flukso.rb
@@ -43,8 +43,8 @@ module Flukso
   class Unavailable < StandardError; end
   class InformFlukso < StandardError; end
   class NotFound < StandardError; end
-
-  BASE_SENSOR_URL = "http://api.flukso.net/sensor"
+  # Use only the encrypted endpoint.
+  BASE_SENSOR_URL = "https://api.flukso.net/sensor"
 end
  
 directory = File.expand_path(File.dirname(__FILE__))
